5414b600a19b6dc960175123b98729f88a14ab12
Tiago Dias de Sousa <tiagodiassk8@gmail.com>
2022-05-09 15:26:27 -0300

    Projeto Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ambiente-angular';
+
+  count = 0;
+
+  pessoas = [
+    {
+      nome: "Tadeu",
+      sobrenome: "Shimit"
+    },
+     {
+      nome: "Mario",
+      sobrenome: "Brothers"
+    },
+    {
+      nome: "Sonic",
+      sobrenome: "& Tales"
+    }
+  ]
+
+  constructor() { }
+
+  ngOnInit(): void {
+    let interval = setInterval(() => {
+      this.count++;
+      if (this.count === 10) {
+        clearInterval(interval)
+      }
+    }, 1000)
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MarcaTextoDirective } from './directives/marca-texto.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MarcaTextoDirective
   ],
   imports: [
     BrowserModule
